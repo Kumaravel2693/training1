1375367d26bdc95cd6ce6b786034eb446b608e31
Kumaravel2693 <kumaravelramesh93@gmail.com>
2022-02-24 12:06:42 +0530

    pushingcode

---
+++ src/org/product/Sample.java
@@ -0,0 +1,13 @@
+package org.product;
+
+public class Sample {
+	public void a1() {
+		System.out.println("parent");
+
+	}
+  }
+		
+
+
+
+
---
+++ src/org/product/Sample1.java
@@ -0,0 +1,13 @@
+package org.product;
+
+public class Sample1 {
+	private void a2() {
+		
+}
+	
+
+	public static void main(String[] args) {
+		Sample1 s=new Sample1();
+	}
+	
+}
